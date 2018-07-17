8030906420ae07cf99fd1706929fb185726f3eb9
Paul Loyd <pavelko95@gmail.com>
2018-07-17 11:55:58 +0300

    test(matcher): add numeric tests

--- src/matcher/spec.rs
+++ src/matcher/spec.rs
@@ -2,6 +2,7 @@ use super::*;
 
 mod subtype {
     use schema::RcMixed;
+    use unit::Point;
 
     macro_rules! test {
         ([$field:ident] $child:expr, $parent:expr => $res:expr) => {{
@@ -22,6 +23,24 @@ mod subtype {
         }};
     }
 
+    macro_rules! test_max {
+        ($field:ident) => {
+            test!([$field] None, Some(42) => false);
+            test!([$field] Some(42), None => true);
+            test!([$field] Some(42), Some(43) => true);
+            test!([$field] Some(42), Some(41) => false);
+        };
+    }
+
+    macro_rules! test_min {
+        ($field:ident) => {
+            test!([$field] None, Some(42) => false);
+            test!([$field] Some(42), None => true);
+            test!([$field] Some(42), Some(43) => false);
+            test!([$field] Some(42), Some(41) => true);
+        };
+    }
+
     #[test]
     fn it_should_check_const() {
         test!([const_] None, Some(RcMixed::from(42)) => false);
@@ -36,5 +55,61 @@ mod subtype {
         test!([multiple_of] Some(42.), Some(42.) => true);
         test!([multiple_of] Some(84.), Some(42.) => true);
         test!([multiple_of] Some(83.), Some(42.) => false);
+    }
+
+    #[test]
+    fn it_should_check_maximum() {
+        test!([maximum] None, Some(Point::exc(42.)) => false);
+        test!([maximum] Some(Point::exc(42.)), None => true);
+        test!([maximum] Some(Point::exc(42.)), Some(Point::exc(42.)) => true);
+        test!([maximum] Some(Point::inc(42.)), Some(Point::inc(42.)) => true);
+        test!([maximum] Some(Point::exc(42.)), Some(Point::inc(42.)) => true);
+        test!([maximum] Some(Point::inc(42.)), Some(Point::exc(42.)) => false);
+        test!([maximum] Some(Point::inc(42.)), Some(Point::exc(43.)) => true);
+        test!([maximum] Some(Point::inc(42.)), Some(Point::inc(43.)) => true);
+        test!([maximum] Some(Point::inc(43.)), Some(Point::inc(42.)) => false);
+    }
+
+    #[test]
+    fn it_should_check_minimum() {
+        test!([minimum] None, Some(Point::exc(42.)) => false);
+        test!([minimum] Some(Point::exc(42.)), None => true);
+        test!([minimum] Some(Point::exc(42.)), Some(Point::exc(42.)) => true);
+        test!([minimum] Some(Point::inc(42.)), Some(Point::inc(42.)) => true);
+        test!([minimum] Some(Point::exc(42.)), Some(Point::inc(42.)) => true);
+        test!([minimum] Some(Point::inc(42.)), Some(Point::exc(42.)) => false);
+        test!([minimum] Some(Point::inc(42.)), Some(Point::exc(43.)) => false);
+        test!([minimum] Some(Point::inc(42.)), Some(Point::inc(43.)) => false);
+        test!([minimum] Some(Point::inc(43.)), Some(Point::inc(42.)) => true);
+    }
+
+    #[test]
+    fn it_should_check_max_length() {
+        test_max!(max_length);
+    }
+
+    #[test]
+    fn it_should_check_min_length() {
+        test_min!(min_length);
+    }
+
+    #[test]
+    fn it_should_check_max_items() {
+        test_max!(max_items);
+    }
+
+    #[test]
+    fn it_should_check_min_items() {
+        test_min!(min_items);
+    }
+
+    #[test]
+    fn it_should_check_max_properties() {
+        test_max!(max_properties);
+    }
+
+    #[test]
+    fn it_should_check_min_properties() {
+        test_min!(min_properties);
     }
 }
